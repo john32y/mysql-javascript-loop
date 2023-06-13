150eea870a84856d9327ffba3f76425929cf5447
john32y <john.thelronge@gmail.com>
2023-06-13 16:47:32 -0400

    FIRST COMMIT

---
+++ index.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql2';
+import { dbCredentials } from './secrets.js';
+
+const db = mysql.createConnection(dbCredentials);
+
+const query = "SELECT * FROM movieTable";
+
+db.query(
+    query,
+    function(err, results) {
+    console.log(err);
+    console.table(results);
+
+    for ( let i=0; i<results.length; i++) {
+        console.log(results[i]);
+    }
+
+db.destroy();
+
+    }
+)
